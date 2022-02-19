a3e122d6e0dc8f41268a1dc82e9fd07ae363679b
ahmed <artistahmed41@gmail.com>
2022-02-20 04:52:02 +0500

    model wallet, field raddress (succesful on admin)

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Wallet(models.Model):
+    raddress = models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
