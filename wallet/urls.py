ce3eecd7ef1410f3669261f3f28f11f244cc08e8
ahmed <artistahmed41@gmail.com>
2022-02-28 13:47:51 +0500

    modified paths and post

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('', views.getData),
     path('item/<str:pk>/', views.getSingleData, name="item"),
     path('create/', views.postData, name="create"),
-
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from .serializers import WalletSerializer
 from .models import Wallet
 
+
 @api_view(['GET'])
 def getData(request):
     queryset = Wallet.objects.all()
