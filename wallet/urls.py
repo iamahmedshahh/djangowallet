6422d882f16865b43e665fc9936363e3d35b942e
ahmed <artistahmed41@gmail.com>
2022-03-01 15:53:27 +0500

    crud functions complete and working

--- wallet/urls.py
+++ wallet/urls.py
@@ -2,7 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.getData),
-    path('item/<str:pk>/', views.getSingleData, name="item"),
-    path('create/', views.postData, name="create"),
+    path('',views.getData),
+    path('item/<str:pk>/',views.getSingleData, name="item"),
+    path('create/',views.postData, name="create"),
+    path('update/<str:pk>/',views.updateData, name="update"),
+    path('delete/<str:pk>/',views.deleteData, name="delete"),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -6,13 +6,13 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def getData(request):
     queryset = Wallet.objects.all()
-    serializer = WalletSerializer(queryset, many=True, context={'request': request})
+    serializer = WalletSerializer(queryset,many=True,context={'request': request})
     return Response(serializer.data)
 
 @api_view(['GET'])
 def getSingleData(request,pk):
-    queryset = Wallet.objects.all(id=pk)
-    serializer = WalletSerializer(queryset, many=False)
+    queryset = Wallet.objects.get(id=pk)
+    serializer = WalletSerializer(queryset,many=False)
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -22,3 +22,18 @@ def postData(request):
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors)
+
+@api_view(['POST'])
+def updateData(request, pk):
+    queryset = Wallet.objects.get(id=pk)
+    data = WalletSerializer(instance=queryset, data=request.data)  
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    return Response(queryset.errors)
+
+@api_view(['DELETE'])
+def deleteData(request, pk):
+	queryset = Wallet.objects.get(id=pk)
+	queryset.delete()
+	return Response(queryset)
